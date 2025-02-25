9ed66302d49648f1fa543b1424dfcef006815fb8
Pkwaks <Pascal_Kwaks@hotmail.com>
2025-02-25 17:05:46 +0100

    Cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,39 +6,13 @@ import type { EyesFixture } from '@applitools/eyes-playwright/types/fixture';
  */
 export default defineConfig<EyesFixture>({
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 5 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: [
-  //   ['@applitools/eyes-playwright/reporter', { open: 'never' }],
-  //   ['junit', { outputFile: '../../playwright-report/report.xml' }]
-  // ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  workers: 5,
   use: {
     /* Configuration for Eyes VisualAI */
     eyesConfig: {
       serverUrl: 'https://nsceyes.applitools.com/',
-      failTestsOnDiff: 'afterEach',
-      appName: 'CMS Frontend',
-      matchLevel: 'Strict',
-      layoutBreakpoints: [320, 480, 740, 980, 1300],
-      branchName: "testBranch",
-      testConcurrency: 75,
-      saveNewTests: false,
-      type: 'ufg',
-      waitBeforeCapture: 1000
-    },
-
-    baseURL: process.env.BASE_URL,
-    navigationTimeout: 30000,
-    trace: {
-      mode: 'retain-on-failure'
+      appName: 'Example'
     }
   },
 
